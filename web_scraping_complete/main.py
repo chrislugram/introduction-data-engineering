68f5cd863bf8eef54f81e4254f287396d51c91c6
Christian Lugo Ramírez <chrislugram@gmail.com>
2023-05-10 23:36:06 +0200

    feat(main): Update main with exchange rate module

--- web_scraping_complete/main.py
+++ web_scraping_complete/main.py
@@ -2,7 +2,12 @@
 Main file of all sample
 """
 from bank_market_cap import get_bank_market_cap
+from exchange_data import get_exchange_data
+
 
 if __name__ == "__main__":
-    df = get_bank_market_cap()
-    df.to_json("bank_market_cap.json")
+    # df = get_bank_market_cap()
+    # df.to_json("bank_market_cap.json")
+
+    df = get_exchange_data()
+    df.to_csv("exanghe_rates_1.csv", index=True)
