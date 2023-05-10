3d8effef720d6afce82c9d5c7100f703b57648bd
Christian Lugo Ramírez <chrislugram@gmail.com>
2023-05-10 23:36:43 +0200

    fix(main): Fix typo for file

--- web_scraping_complete/main.py
+++ web_scraping_complete/main.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     # df.to_json("bank_market_cap.json")
 
     df = get_exchange_data()
-    df.to_csv("exanghe_rates_1.csv", index=True)
+    df.to_csv("exchange_rates_1.csv", index=True)
